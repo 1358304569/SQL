3e67fe35f9692a4be2201bfbdfc557dd1feb9064
zhouyijian <zhouyijian@nonobank.com>
2019-05-05 15:28:29 +0800

    查找最晚入职的员工信息

---
+++ 牛客网SQL实战练习_1.sql
@@ -0,0 +1,21 @@
+
+/*
+1.查找最晚入职员工的所有信息
+CREATE TABLE `employees` (
+`emp_no` int(11) NOT NULL,
+`birth_date` date NOT NULL,
+`first_name` varchar(14) NOT NULL,
+`last_name` varchar(16) NOT NULL,
+`gender` char(1) NOT NULL,
+`hire_date` date NOT NULL,
+PRIMARY KEY (`emp_no`));
+*/
+
+    select *
+      from employees t1
+     where t1.hire_date = (select max(t2.hire_date) from employees t2)
+
+
+
+
+
